807d87bd4b0e635581d67c3b8afa8918c769e45e
azorinov <azorinov@hotmail.com>
2023-09-27 19:53:50 +0300

    toolbar

---
+++ NavigObjC/TwoViewController.h
@@ -0,0 +1,16 @@
+//
+//  TwoViewController.h
+//  NavigObjC
+//
+//  Created by Alexander Zorinov on 2023-09-27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TwoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NavigObjC/TwoViewController.m
@@ -0,0 +1,31 @@
+//
+//  TwoViewController.m
+//  NavigObjC
+//
+//  Created by Alexander Zorinov on 2023-09-27.
+//
+
+#import "TwoViewController.h"
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
