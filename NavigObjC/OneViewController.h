4ef92d4d52c8f3604a2eefed522ca459bf67a4b2
azorinov <azorinov@hotmail.com>
2023-09-27 21:01:52 +0300

    popover

--- NavigObjC/OneViewController.h
+++ NavigObjC/OneViewController.h
@@ -6,11 +6,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TwoViewController.h"
 
-NS_ASSUME_NONNULL_BEGIN
+@interface OneViewController : UIViewController <UIPopoverPresentationControllerDelegate>
 
-@interface OneViewController : UIViewController
+@property (nonatomic, strong) IBOutlet TwoViewController    *twoViewController;
 
 @end
 
-NS_ASSUME_NONNULL_END
--- NavigObjC/OneViewController.m
+++ NavigObjC/OneViewController.m
@@ -6,26 +6,71 @@
 //
 
 #import "OneViewController.h"
+#import "TwoViewController.h"
 
 @interface OneViewController ()
+- (IBAction)openPopover:(id)sender;
+- (IBAction)showTwo:(id)sender;
+- (IBAction)openAlert:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *bookmarks;
 @end
 
 @implementation OneViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)openAlert:(id)sender {
+    UIAlertController * alert = [UIAlertController
+                                 alertControllerWithTitle:@"Tennis"
+                                 message:@"Do you want to play tennis?"
+                                 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* yesButton = [UIAlertAction
+                                actionWithTitle:@"Yes"
+                                style:UIAlertActionStyleDefault
+                                handler:^(UIAlertAction * action) {
+        ///block/closure
+    }];
+    UIAlertAction* noButton = [UIAlertAction
+                               actionWithTitle:@"Cancel"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {
+        ///block/closure
+    }];
+    [alert addAction:yesButton];
+    [alert addAction:noButton];
+    
+    [self presentViewController:alert animated:YES completion:nil];
 }
-*/
+
+- (IBAction)showTwo:(id)sender {
+    [self showViewController:[[TwoViewController alloc] init] sender:self];
+}
+
+- (IBAction)openPopover:(id)sender {
+    UIViewController *popViewController = [UIViewController new];
+    popViewController.view.backgroundColor = [UIColor blueColor];
+    popViewController.preferredContentSize = CGSizeMake(300, 300);
+    popViewController.modalPresentationStyle = UIModalPresentationPopover;
+    popViewController.popoverPresentationController.delegate = self;
+    [self presentViewController:popViewController animated:true completion:nil];
+}
+
+#pragma mark - <UIPopoverPresentationControllerDelegate>
+- (UIModalPresentationStyle) adaptivePresentationStyleForPresentationController:(UIPresentationController *)controller {
+    return UIModalPresentationNone;
+}
+
+- (void)prepareForPopoverPresentation:(UIPopoverPresentationController *)popoverPresentationController {
+    popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionAny;
+    popoverPresentationController.barButtonItem = self.bookmarks;
+}
+
+- (void)presentationControllerDidDismiss:(UIPopoverPresentationController *)popoverPresentationController {
+    NSLog(@"Popover Dismissed!");
+}
 
 @end
